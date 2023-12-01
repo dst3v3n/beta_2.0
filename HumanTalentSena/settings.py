1fd0dcde292596983329011eaf2f7e860af37c04
lauraroseroochoa <lauratatianarosero1@gmail.com>
2023-12-01 11:16:51 -0500

    w faltante

--- HumanTalentSena/settings.py
+++ HumanTalentSena/settings.py
@@ -60,7 +60,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.CommonMiddleare'
+    'django.middleware.common.CommonMiddleware'
 ]
 
 ROOT_URLCONF = 'HumanTalentSena.urls'
